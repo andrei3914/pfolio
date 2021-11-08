d0b7c40bb8c96f01c8e27e4dd2c1db00df5dfd2e
andrei3914 <popandrei230@yahoo.com>
2021-11-08 11:46:32 +0200

    schimbat font

--- src/components/Welcome/Welcome.js
+++ src/components/Welcome/Welcome.js
@@ -6,7 +6,7 @@ const Welcome = () => {
 		<div className="acasa center" id="clickeu">
 			<h1 className="eldivo stiltitlu"> {'Octavian Andrei Pop'} </h1>
 			<h2 className="eldivo center" id="typeId"> {'Technology enthusiast'}</h2>
-			<p className="eldivo center f3 subtitle"> {'Welcome to my website.'}</p>
+			<p className="eldivo center f4 subtitle"> {'Welcome to my website.'}</p>
 		</div>
 	);
 }
