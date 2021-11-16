5af650019b4fa6d224253f251eb2fb70cf90b9f5
andrei3914 <popandrei230@yahoo.com>
2021-11-16 13:01:12 +0200

    added 2 projects

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -31,6 +31,28 @@ const Projects = () => {
 			    functionality is provided by the react-speech-recognition hook. The project is deployed on Heroku.
 			  </p>
 			</article></Tilty>
+
+			<Tilty><article className="mw5 center bg-white br3 pa3 pa4-ns mv3 ba b--black-10 shadow5 articleStil">
+			  <div className="tc">
+			    
+			    <a href="https://andrei3914.github.io/mechanicSite/#getMeHome" target="_blank" rel="noreferrer"><h1 className="f4">A mechanic's site</h1></a>
+			    <hr className="mw3 bb bw1 b--black-10" />
+			  </div>
+			  <p className="lh-copy measure center f5 black-70">
+			    Application created for an auto mechanic with React.
+			  </p>
+			</article></Tilty>
+
+			<Tilty><article className="mw5 center bg-white br3 pa3 pa4-ns mv3 ba b--black-10 shadow5 articleStil">
+			  <div className="tc">
+			    
+			    <a href="https://andrei3914.github.io/nietzschesZodiac/" target="_blank" rel="noreferrer"><h1 className="f4">Nietzsche's Zodiac</h1></a>
+			    <hr className="mw3 bb bw1 b--black-10" />
+			  </div>
+			  <p className="lh-copy measure center f5 black-70">
+			    Fun application created with React, based on the genius of the one and only Friedrich Nietzsche.
+			  </p>
+			</article></Tilty>
 			</div>
 		</div>
 	);
