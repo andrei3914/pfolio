7490879dafa1260aa899d902309bbfb0ede485fb
andrei3914 <popandrei230@yahoo.com>
2022-02-16 10:04:17 +0200

    added github profile link

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import './Footer.css';
+import imagine from './github-mark-light.png';
 
 class Footer extends React.Component {
 	render() {
 		return (
 			<footer className="bg-near-black white-80 pv3 pv2-l ph4 position orientare">
-			  <p className="f6">
-			  	<span className="dib mr4 mr5-ns hover-light-purple">Octavian Andrei Pop</span>
-			  </p>
-
+			  	<div className="leftPart">
+					<p className="f6">
+						<span className="dib mr4 mr5-ns hover-light-purple">Octavian Andrei Pop</span>
+					</p>
+					<div className="imagine">
+				    	<a href="https://github.com/andrei3914" target="_blank" rel="noreferrer noopener"><img src={imagine} alt="github logo"></img></a>
+					</div>
+				</div>
 			  <p className="f6 link white-80 hover-light-purple">
 			  	<span>Made with React</span>
 			  </p>
